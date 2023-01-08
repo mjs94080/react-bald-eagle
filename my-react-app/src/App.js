375a3bdaaf7f5d625db0758f30770fa4a08d0fd6
MacKenzie <mjs94080@gmail.com>
2023-01-07 19:58:11 -0800

    lesson-1.7

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,20 +2,47 @@ import React, { useState, useEffect } from "react";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 
-const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem("savedTodoList") || "[]")
-  );
+// const useSemiPersistentState = () => {
+//   const [todoList, setTodoList] = useState(
+//     JSON.parse(localStorage.getItem("savedTodoList") || "[]")
+//   );
 
-  useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
+//   useEffect(() => {
+//     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+//   }, [todoList]);
 
-  return [todoList, setTodoList];
-};
+//   return [todoList, setTodoList];
+// };
 
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
+  const [todoList, setTodoList] = useState([]);
+
+  //create a new state variable named isLoading with update
+  //function named setIsLoading with default value true
+  const [isLoading, setIsLoading] = useState(true);
+
+  //NEW ASYNCH START HERE!!!!!
+  useEffect(() => {
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem("todoList")), //|| "[]",
+          },
+        });
+      }, 2000);
+    }).then((result) => {
+      setTodoList(result.data?.todoList || []);
+      setIsLoading(false);
+    });
+  }, []);
+
+  //NEW
+  useEffect(() => {
+    if (!isLoading) {
+      localStorage.setItem("todoList", JSON.stringify(todoList));
+    }
+  }, [todoList, isLoading]);
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
@@ -32,7 +59,13 @@ function App() {
       <h1>Todo List</h1>
       <hr />
       <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      {isLoading ? (
+        <p>
+          <strong>Busting Loads...</strong>
+        </p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      )}
     </>
   );
 }
